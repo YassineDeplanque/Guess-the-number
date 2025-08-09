fc56841ab6167041da48d694fd1844f89d7b2860
unknown <yassine59958@gmail.com>
2025-08-09 23:34:15 +0200

    verification works

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 let number = Math.floor(Math.random() * 100)
-
+console.log(number)
 
 function submit(){
     let proposition = document.getElementById('input').value
     console.log(proposition)
+    if(proposition == number){
+        console.log('win')
+    } else if(proposition >= number){
+        console.log('too high')
+    } else{
+        console.log('too low')
+    }
 }
